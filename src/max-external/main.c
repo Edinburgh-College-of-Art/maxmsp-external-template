23efc07ebc8dc04ada083a0d5fa76df177708e25
mhamilt <matt.hamilton@ed.ac.uk>
2021-11-24 15:00:01 +0000

    remove msp methods from max template

--- src/max-external/main.c
+++ src/max-external/main.c
@@ -18,12 +18,6 @@
 void* myExternClass;
 
 //------------------------------------------------------------------------------
-/// DSP object properties
-typedef struct _MyDspStruct
-{
-    float a;
-} MyDspStruct;
-
 /** @struct
  The MaxMSP object
  */
@@ -31,7 +25,6 @@ typedef struct _MaxExternalObject
 {
     t_pxobject x_obj;
     t_symbol* x_arrayname;
-    MyDspStruct a;
     short inletConnection;
     double gain;
 } MaxExternalObject;
@@ -40,13 +33,14 @@ typedef struct _MaxExternalObject
 /// Arguement list should be as long as the list of type arguments passed in the class_new call below.
 /// @param arg1 first argument to object: should match type given in class_new(...)
 /// @returns a void* to an instance of the MaxExternalObject
-void* myExternalConstructor(long arg1)
+MaxExternalObject* myExternalConstructor(t_symbol *s, long argc, t_atom *argv)
 {
     //--------------------------------------------------------------------------
-    if (!arg1)
-    {
-        post("no arguement\n");
-    }
+    if (argc)
+        post("Number of Arguments: %ld", argc);
+    else
+        post("No Argument Given");
+    
     //--------------------------------------------------------------------------
     MaxExternalObject* maxObjectPtr = (MaxExternalObject*)object_alloc(myExternClass);
     dsp_setup((t_pxobject*)maxObjectPtr, 1);
@@ -62,7 +56,7 @@ void* myExternalConstructor(long arg1)
 /// @brief what happens when the object is deleted
 void myExternDestructor(MaxExternalObject* maxObjectPtr)
 {
-    post("END");
+    post("Object Destoryed");
     dsp_free((t_pxobject*)maxObjectPtr);
 }
 //------------------------------------------------------------------------------
@@ -118,68 +112,6 @@ void inletAssistant(MaxExternalObject* maxObjectPtr,
     }
 }
 
-//------------------------------------------------------------------------------
-#pragma mark DSP Loop
-/// Main DSP process block, do your DSP here
-/// @param maxObjectPtr
-/// @param dsp64
-/// @param ins double pointer array to sample inlets
-/// @param numins
-/// @param outs double pointer array to sample outlets
-/// @param numouts
-/// @param sampleframes samples per channel
-/// @param flags
-/// @param userparam no idea
-void mspExternalProcessBlock(MaxExternalObject* maxObjectPtr, t_object* dsp64,
-                             double** ins, long numins, double** outs, long numouts,
-                             long sampleframes, long flags, void* userparam)
-
-{
-    //--------------------------------------------------------------------------
-    // pass through
-    double* in = ins[0];
-    
-    if(!maxObjectPtr->inletConnection)
-    {
-        for (int s = 0; s < sampleframes; ++s)
-        {
-            in[s] = 0.0;
-        }
-    }
-    
-    //--------------------------------------------------------------------------
-    // DSP loops
-    
-    for (int s = 0; s < sampleframes; ++s)
-    {
-        outs[0][s] = tanh(in[s] * maxObjectPtr->gain);
-    }
-    for (int i = 1; i < numouts; ++i)
-    {
-        outs[i] = outs[0];
-    }
-}
-//------------------------------------------------------------------------------
-
-/// Audio DSP setup
-/// @param maxObjectPtr object pointer
-/// @param dsp64
-/// @param count array containing number of connections to an inlet with index [i]
-/// @param samplerate
-/// @param vectorsize
-/// @param flags
-void prepareToPlay(MaxExternalObject* maxObjectPtr, t_object* dsp64, short* count,
-                   double samplerate, long vectorsize, long flags)
-{
-    maxObjectPtr->inletConnection = count[0];
-    
-    object_method(dsp64,
-                  gensym("dsp_add64"),
-                  maxObjectPtr,
-                  mspExternalProcessBlock,
-                  0,
-                  NULL);
-}
 //------------------------------------------------------------------------------
 
 /// This gets called when we receive a bang
@@ -245,20 +177,19 @@ void coupleMethodsToExternal( t_class* c)
     class_addmethod(c, (method)onFloat, "float", A_FLOAT, 0);
     class_addmethod(c, (method)inletAssistant,"assist", A_CANT,0);
     class_addmethod(c, (method)onPrintMessage, "print", 0);
-    class_addmethod(c, (method)onAnyMessage, "anything", A_GIMME, 0);
-    class_addmethod(c, (method)prepareToPlay, "dsp64", A_CANT, 0);
+    class_addmethod(c, (method)onAnyMessage, "anything", A_GIMME, 0);    
 }
 //------------------------------------------------------------------------------
 int C74_EXPORT main(void)
 {
-    post("Hello Max External!");
+    post("Hello Max!");
     
     t_class* c = class_new("max-external",
                            (method)myExternalConstructor,
                            (method)myExternDestructor,
                            (short)sizeof(MaxExternalObject),
                            0L,
-                           A_DEFLONG,
+                           A_GIMME,
                            0);
     
     coupleMethodsToExternal(c);
